b1a0b22f6878081da20aaecb15252dab092ad431
Imran Thoufeeque <75982271+I7Z3R0@users.noreply.github.com>
2021-11-12 10:35:54 +0530

    Create Insp3ct0r.py

---
+++ PICO CTF/2019/Web/Insp3ct0r/Insp3ct0r.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from pwn import *
+import requests
+import re
+
+url = "https://jupiter.challenges.picoctf.org/problem/41511/"
+session = requests.session()
+content = session.get(url).text
+web_first = re.findall('Anyways have 1/3 of the flag: (.*) -->', content)
+first = "".join(web_first)
+
+url2 = "https://jupiter.challenges.picoctf.org/problem/41511/mycss.css"
+content2 = session.get(url2).text
+web_second = re.findall("Here's part 2/3 of the flag: (.*) ", content2)
+second = "".join(web_second)
+
+url3 = "https://jupiter.challenges.picoctf.org/problem/41511/myjs.js"
+content3 = session.get(url3).text
+web_third = re.findall("Anyways part 3/3 of the flag: (.*) ", content3)
+third = "".join(web_third)
+
+sleep(3)
+log.progress("Kindly wait until we get te flag for you!")
+
+flag = first+second+third
+print(flag)
